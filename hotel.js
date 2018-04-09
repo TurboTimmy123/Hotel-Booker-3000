31f787808b2e4aaeedeb997688b7a8089fc3f1e7
TurboTimmy123 <TurboTimmy123@example.com>
2018-04-09 12:36:37 -0700

    Added login window + Reviews + Background RGB

--- hotel.js
+++ hotel.js
@@ -20,17 +20,38 @@ $(window).scroll(function() {
 var t = 1;
 var temp;
 
+var ourFloatingWindows;
+var ourButtons;
+
 function colorLoop() {
   temp = document.getElementsByTagName("button");
   console.log("got: " + temp);
+  ourFloatingWindows = document.getElementsByClassName("floatingWindow");
+  ourButtons = document.getElementsByClassName("button");
   setInterval(loop, 100);
+}
+
+function counterLoop() {
+  setInterval(fakeCounterIncrementer, 10);
+}
+
+function fakeCounterIncrementer() {
+  document.getElementById("fakeCounter").textContent++;
 }
 
 function loop() {
   t++;
-  document.getElementById("fakeCounter").textContent++;
   //temp.style.color = white;
-  $("#epicButton").css("background-color", getRainboxHex(t));
+  //$("#epicButton").css("background-color", getRainboxHex(t / 2));
+  $("#theBackground").css("background-color", getRainboxHex(t / 2, 0.1));
+
+  for (var i = 0; i < ourFloatingWindows.length; i++) {
+    $(ourFloatingWindows[i]).css("border-color", getRainboxHex(t / 2, 1));
+  }
+
+  for (var j = 0; j < ourButtons.length; j++) {
+    $(ourButtons[j]).css("background-color", getRainboxHex(t / 2, 1));
+  }
 
   if ((Math.random() * 1000) > 999) {
     if (confirm("CONGRATULATIONS, You just won a free iPad, click OK to claim prize!!!")) {
@@ -54,10 +75,37 @@ function clamp(x, min, max) {
   return x;
 }
 
-function getRainboxHex(t) {
+var lol;
+
+function getRainboxHex(t, opacity) {
   var red = Math.sin(t + 0) * 127 + 128;
   var green = Math.sin(t + 2) * 127 + 128;
   var blue = Math.sin(t + 4) * 127 + 128;
-  var lol = "RGB(" + red + "," + green + "," + blue + ")";
+
+  lol = "rgba(" + red + "," + green + "," + blue + "," + opacity + ")";
+
+
   return lol;
+}
+
+function showLoginScreen() {
+  console.log("Open login");
+  document.getElementById("loginPopup").style.display = 'block';
+}
+
+function hideLoginScreen() {
+  console.log("close login");
+  document.getElementById("loginPopup").style.display = 'none';
+}
+
+function addReview() {
+  console.log("Adding Review...");
+  var ourText = document.getElementById("msg");
+  console.log(ourText.value);
+
+  $("#reviews").append("<div class=\"reviews\">" + ourText.value + "</div><hr>");
+}
+
+function userLogOut() {
+  alert("User attempted to log out");
 }
